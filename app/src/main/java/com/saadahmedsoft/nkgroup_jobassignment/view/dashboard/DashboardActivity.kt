6ca2bbe63df841b081b1f31c364bd912ed6761ee
Saad <saadahmedscse@gmail.com>
2022-08-29 23:49:36 +0600

    Bottom nav bar tasks done

--- app/src/main/java/com/saadahmedsoft/nkgroup_jobassignment/view/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/saadahmedsoft/nkgroup_jobassignment/view/dashboard/DashboardActivity.kt
@@ -1,7 +1,10 @@
 package com.saadahmedsoft.nkgroup_jobassignment.view.dashboard
 
 import android.os.Bundle
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment
 import com.saadahmedsoft.base.BaseActivity
+import com.saadahmedsoft.nkgroup_jobassignment.R
 import com.saadahmedsoft.nkgroup_jobassignment.databinding.ActivityDashboardBinding
 import com.saadahmedsoft.nkgroup_jobassignment.databinding.AppToolbarBinding
 
@@ -11,7 +14,37 @@ class DashboardActivity : BaseActivity<ActivityDashboardBinding>(ActivityDashboa
         get() = binding.appToolbar
 
     override fun onActivityCreate(savedInstanceState: Bundle?) {
-        //
+        val myNavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = myNavHostFragment.navController
+
+        val options = NavOptions.Builder()
+            .setLaunchSingleTop(true)
+            .setEnterAnim(com.saadahmedsoft.shortintent.R.anim.animate_swipe_left_enter)
+            .setExitAnim(com.saadahmedsoft.shortintent.R.anim.animate_swipe_left_exit)
+            .setPopEnterAnim(com.saadahmedsoft.shortintent.R.anim.animate_swipe_right_enter)
+            .setPopExitAnim(com.saadahmedsoft.shortintent.R.anim.animate_swipe_right_exit)
+            .setPopUpTo(navController.graph.startDestinationId, false)
+            .build()
+
+        binding.navView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.roomFragment -> {
+                    if (toolbarBinding.toolbarTitle.text != "SQ Lite") {
+                        navController.navigate(R.id.roomFragment, null, options)
+                    }
+                }
+                R.id.restApiFragment -> {
+                    if (toolbarBinding.toolbarTitle.text != "Rest API") {
+                        navController.navigate(R.id.restApiFragment, null, options)
+                    }
+                }
+            }
+            true
+        }
+    }
+
+    fun setNavButton() {
+        binding.navView.selectedItemId = R.id.roomFragment
     }
 
     override fun observeData() {}
--- app/src/main/java/com/saadahmedsoft/nkgroup_jobassignment/view/dashboard/tabs/sqLite/RoomFragment.kt
+++ app/src/main/java/com/saadahmedsoft/nkgroup_jobassignment/view/dashboard/tabs/sqLite/RoomFragment.kt
@@ -1,9 +1,13 @@
 package com.saadahmedsoft.nkgroup_jobassignment.view.dashboard.tabs.sqLite
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.saadahmedsoft.base.BaseFragment
 import com.saadahmedsoft.base.utils.Constants.Booleans.FALSE
 import com.saadahmedsoft.nkgroup_jobassignment.databinding.FragmentRoomBinding
+import com.saadahmedsoft.nkgroup_jobassignment.view.dashboard.DashboardActivity
 
 class RoomFragment : BaseFragment<FragmentRoomBinding>(FragmentRoomBinding::inflate) {
 
@@ -14,6 +18,15 @@ class RoomFragment : BaseFragment<FragmentRoomBinding>(FragmentRoomBinding::infl
 
     override fun onFragmentCreate(savedInstanceState: Bundle?) {
         //
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        (activity as DashboardActivity).setNavButton()
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
     override fun observeData() {}
