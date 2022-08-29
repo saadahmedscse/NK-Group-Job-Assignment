6838c612ab189312d722aa62599158be33fb7edb
Saad <saadahmedscse@gmail.com>
2022-08-30 05:02:33 +0600

    Finish

--- app/src/main/java/com/saadahmedsoft/nkgroup_jobassignment/view/splash/SplashActivity.kt
+++ app/src/main/java/com/saadahmedsoft/nkgroup_jobassignment/view/splash/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.saadahmedsoft.nkgroup_jobassignment.view.splash
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.saadahmedsoft.base.helper.delay
 import com.saadahmedsoft.nkgroup_jobassignment.R
 import com.saadahmedsoft.nkgroup_jobassignment.view.dashboard.DashboardActivity
@@ -13,9 +14,12 @@ import com.saadahmedsoft.shortintent.ShortIntent
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         setContentView(R.layout.activity_splash)
 
-        delay(500) {
+        delay(1500) {
             ShortIntent.getInstance(this)
                 .addDestination(DashboardActivity::class.java)
                 .addTransition(Anim.FADE)
